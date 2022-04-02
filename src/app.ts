bb4a2b9347b4e281407afaef8c1b208b6558c76c
rakitenkogermans <rakitenko.germans@gmail.com>
2022-04-02 21:05:42 +0300

    rendering lists

--- src/app.ts
+++ src/app.ts
@@ -39,6 +39,33 @@ const autobind = (target: any, methodName: string, descriptor: PropertyDescripto
     return adjDescriptor;
 }
 
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.querySelector('#project-list')! as HTMLTemplateElement;
+        this.hostElement = document.querySelector('#app')! as HTMLDivElement;
+
+        const importNode = document.importNode(this.templateElement.content, true);
+        this.element = importNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -91,6 +118,7 @@ class ProjectInput {
             alert('Invalid input, please try again!');
             return;
         }
+
         return [enteredTitle, enteredDescription, +enteredPeople];
     }
 
@@ -120,3 +148,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const active = new ProjectList('active');
+const finished = new ProjectList('finished');
